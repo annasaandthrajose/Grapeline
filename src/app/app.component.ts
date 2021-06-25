7d0fe1ba80e5dd874af528e591247f7ac8ca5585
annasaandthrajose <annasaandthrajose@gmail.com>
2021-06-25 10:25:38 +0530

    todo client angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
-
+import {Todo}from './todo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+    
+      
+    }
+    
+    todos=[];//this is empty array
+    
+  name='';
+   editable= false
+    addButton="ADD"
+
+   editableIndex = 0
+
+  Add(){
+    if(this.editable === true)
+    {
+      const Todoslist:Todo={
+        name:this.name,
+        editable:this.editable
+      }
+      this.todos.splice(this.editableIndex,1,Todoslist) 
+      this.addButton="ADD"
+      this.editable= false
+    }
+    else{
+      const Todoslist:Todo={
+        name:this.name,
+        editable:this.editable
+      }
+      this.todos.splice(0,0,Todoslist)
+      this.name=''
+    }
+    
+  }
+
+Delete(index){
+this.todos.splice(index,1)
+}
+Edit(item,index){
+  this.editable= true
+this.addButton="UPDATE"
+this.name=item;
+this.editableIndex=index
+}
 }
